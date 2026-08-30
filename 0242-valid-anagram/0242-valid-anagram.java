8a3aa09ce07bdc5ee38b2b43b943070fd4c85cd0
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-08-30 22:35:04 +0530

    Update 0242-valid-anagram.java

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,1 +1,29 @@
+import java.util.HashMap;
 
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char cht = t.charAt(i);
+            if (!map.containsKey(cht)) {
+                return false;
+            }
+            map.put(cht, map.get(cht) - 1);
+            if (map.get(cht) == 0) {
+                map.remove(cht);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
