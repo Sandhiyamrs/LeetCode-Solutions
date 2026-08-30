59435ebc399534476c7234439870ea4a73f8bfbc
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-08-30 22:36:46 +0530

    Update 2965-find-missing-and-repeated-values.java

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int size = n * n;
+        int[] count = new int[size + 1];
+        int repeat = -1, missing = -1;
 
+        // Count occurrences
+        for (int[] row : grid) {
+            for (int num : row) {
+                count[num]++;
+            }
+        }
+
+        // Find the repeat (count == 2) and missing (count == 0)
+        for (int i = 1; i <= size; i++) {
+            if (count[i] == 2) {
+                repeat = i;
+            } else if (count[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{repeat, missing};
+    }
+}
