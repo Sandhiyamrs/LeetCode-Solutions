9eceb73f320b639f298c484b06a946e4cb20b0e8
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-08-30 22:35:57 +0530

    Update 0287-find-the-duplicate-number.java

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,1 +1,16 @@
+import java.util.HashSet;
 
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for (int num : nums) {
+            if (hs.contains(num)) {
+                return num;
+            }
+            hs.add(num);
+        }
+        
+        return -1;
+    }
+}
